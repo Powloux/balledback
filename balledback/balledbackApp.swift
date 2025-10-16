a466a4b095a6632db1f81556c459b91c547c23f6
Powloux <jamesperrow96@gmail.com>
2025-10-16 15:38:46 -0600

    Initial Commit

---
+++ balledback/balledbackApp.swift
@@ -0,0 +1,17 @@
+//
+//  balledbackApp.swift
+//  balledback
+//
+//  Created by James Perrow on 10/16/25.
+//
+
+import SwiftUI
+
+@main
+struct balledbackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
